3548ccccd7c888ac65fc2daf9426bbc6a5e18047
doppl3r <deben3@gmail.com>
2017-07-13 14:53:12 -0700

    first commit

---
+++ passport-to-paradise.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @package Passport_To_Paradise
+ * @version 0.1
+ */
+/*
+Plugin Name: Passport to Paradise
+Plugin URI: https://wordpress.org/plugins/passport-to-paradise
+Description: This plugin allows users to quickly view their Passport to Paradise reward points. Special users are able to edit a full list of rewards.
+Version: 0.1
+Author: Jacob DeBenedetto
+Author URI: http://doppl3r.com
+License: GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: passport-to-paradise
+Passport to Paradise is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+ 
+Passport to Paradise is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License
+along with Passport to Paradise. If not, see {License URI}.
+*/
+
+require_once('admin/ptp-admin-page.php'); 
+
+/* function go below */
+
+
+?>
---
+++ readme.txt
@@ -0,0 +1,11 @@
+=== Passport to Paradise ===
+Contributors: doppl3r
+Tags: edit, table
+Requires at least: 4.8
+Tested up to: 4.8
+Stable tag: 4.8
+
+Quickly edit or read your Passport to Paradise reward points.
+
+== Description ==
+This plugin allows users to quickly view their Passport to Paradise reward points. Special users are able to edit a full list of rewards.
