cf246902f7394d957b021a55233bc78c04c89e85
doppl3r <deben3@gmail.com>
2017-07-14 09:15:31 -0700

    include basic db table

--- includes/ptp-activator.php
+++ includes/ptp-activator.php
@@ -1,6 +1,34 @@
 <?php
 class Ptp_Activator {
 	public static function activate() {
+		global $wpdb;
+		$version = get_option( 'my_plugin_version', '1.0' );
+		$charset_collate = $wpdb->get_charset_collate();
+		$table_name = $wpdb->prefix . 'ptp_table';
 
+		$sql = "CREATE TABLE $table_name (
+			id mediumint(9) NOT NULL AUTO_INCREMENT,
+			time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+			views smallint(5) NOT NULL,
+			clicks smallint(5) NOT NULL,
+			UNIQUE KEY id (id)
+		) $charset_collate;";
+
+		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+		dbDelta( $sql );
+		
+		if ( version_compare( $version, '2.0' ) < 0 ) {
+			$sql = "CREATE TABLE $table_name (
+			id mediumint(9) NOT NULL AUTO_INCREMENT,
+			time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
+			views smallint(5) NOT NULL,
+			clicks smallint(5) NOT NULL,
+			blog_id smallint(5) NOT NULL,
+			UNIQUE KEY id (id)
+			) $charset_collate;";
+			dbDelta( $sql );
+		
+			update_option( 'my_plugin_version', '2.0' );
+		}
 	}
 }
--- uninstall.php
+++ uninstall.php
@@ -1,5 +1,11 @@
 <?php
-// If uninstall not called from WordPress, then exit.
-if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
-	exit;
-}
+// if uninstall.php is not called by WordPress, die
+if (!defined('WP_UNINSTALL_PLUGIN')) { die; }
+ 
+$option_name = 'wporg_option';
+delete_option($option_name);
+// for site options in Multisite
+delete_site_option($option_name);
+// drop a custom database table
+global $wpdb;
+$wpdb->query("DROP TABLE IF EXISTS {$wpdb->prefix}ptp_table");
